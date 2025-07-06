af3c111ad22699d68e295eafa0bb4651aa6e7fa7
REQUIN-V <piripinalejandro@gmail.com>
2025-07-07 00:49:09 +0800

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,11 +4,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dbFile = path.resolve(__dirname, '../db.json');
+const dbFile = path.resolve(process.cwd(), 'data/db.json');  // Save to global location
 const adapter = new JSONFile(dbFile);
 const db = new Low(adapter);
 
-// Initialize database
+// Initialize DB
 await db.read();
 db.data ||= { users: [], characters: [], attacks: [], settings: {} };
 await db.write();
