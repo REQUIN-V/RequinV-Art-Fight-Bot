038beae92556f63a0d4dff1d04870848ba71a840
REQUIN-V <piripinalejandro@gmail.com>
2025-07-06 23:24:33 +0800

    Update set-theme.js

--- commands/set-theme.js
+++ commands/set-theme.js
@@ -1,1 +1,25 @@
-import content from canvas
+export default {
+  name: 'set-theme',
+  description: 'Set the theme name for the current Art Fight event (mod-only). Usage: !set-theme <theme name>',
+  async execute(message, args) {
+    if (!message.member.permissions.has('ManageGuild')) {
+      return message.reply('❌ You do not have permission to use this command.');
+    }
+
+    const themeName = args.join(' ');
+
+    if (!themeName) {
+      return message.reply('Usage: !set-theme <theme name>');
+    }
+
+    const db = (await import('../utils/db.js')).getDB();
+    await db.read();
+
+    db.data.settings = db.data.settings || {};
+    db.data.settings.theme = themeName;
+
+    await db.write();
+
+    message.channel.send(`🎨 Theme set to: **${themeName}**`);
+  }
+};
