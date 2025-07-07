62d090967b730ac9f35bcbdcbc21f86d9813739a
REQUIN-V <hayazameharu@gmail.com>
2025-07-07 16:54:43 +0800

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,9 @@
 // Example command loader
 import setBanner from './set-banner.js';
-import editBanner from './edit-banner.js';
+// import editBanner from './edit-banner.js'; ❌ Remove this line
 
 export const commands = {
   'set-banner': setBanner,
-  'edit-banner': editBanner,
+  // 'edit-banner': editBanner, ❌ Remove this line too
   // other commands...
 };
