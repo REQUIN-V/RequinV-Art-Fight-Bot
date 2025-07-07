bcd93346c00486ddb13a3b4797e37c603b44a767
REQUIN-V <hayazameharu@gmail.com>
2025-07-07 22:23:58 +0800

    Update set-log-channel.js

--- commands/set-log-channel.js
+++ commands/set-log-channel.js
@@ -1,3 +1,5 @@
+import { getDB } from '../utils/db.js';
+
 export default {
   name: 'set-log-channel',
   description: 'Set the current channel as the log channel for attacks (mod-only).',
@@ -6,14 +8,25 @@ export default {
       return message.reply('❌ You do not have permission to use this command.');
     }
 
-    const db = (await import('../utils/db.js')).getDB();
+    const guildId = message.guild.id;
+
+    const db = getDB();
     await db.read();
 
-    db.data.settings = db.data.settings || {};
-    db.data.settings.logChannel = message.channel.id;
+    // Ensure server structure exists
+    db.data.servers = db.data.servers || {};
+    db.data.servers[guildId] = db.data.servers[guildId] || {
+      users: [],
+      attacks: [],
+      defends: [],
+      settings: {},
+      teams: []
+    };
 
+    // Set log channel for this server
+    db.data.servers[guildId].settings.logChannel = message.channel.id;
     await db.write();
 
-    message.channel.send(`📌 This channel has been set as the **attack log channel**.`);
+    message.channel.send(`📌 This channel has been set as the **attack log channel** for this server.`);
   }
 };
