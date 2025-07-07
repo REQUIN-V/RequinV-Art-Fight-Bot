fb1bced84e4e5fb84b52d809b173db8040730728
REQUIN-V <hayazameharu@gmail.com>
2025-07-07 16:04:08 +0800

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,9 @@
+// Example command loader
+import setBanner from './set-banner.js';
+import editBanner from './edit-banner.js';
+
+export const commands = {
+  'set-banner': setBanner,
+  'edit-banner': editBanner,
+  // other commands...
+};
