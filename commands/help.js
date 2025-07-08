60850b4ed3a1661e8966dbbd5e583b17f71ec7f6
REQUIN-V <hayazameharu@gmail.com>
2025-07-08 08:28:09 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,8 @@ export default {
       color: 0xff9ecb,
       description:
         '📘 You can view the full bot documentation and usage guide here:\n' +
-        '[**Profic Art Royal – Google Docs**](https://docs.google.com/document/d/1FtqzjUpLgx8J4nWUxm-aMWzbo7msvnl6vIbzQKvUhL8/preview?tab=t.0#heading=h.dlrvqsooc0h)',
+        '[**Profic Art Royal – Google Docs**](https://docs.google.com/document/d/1FtqzjUpLgx8J4nWUxm-aMWzbo7msvnl6vIbzQKvUhL8/preview?tab=t.0#heading=h.dlrvqsooc0h)\n\n' +
+        '> ⚠️ **Disclaimer:** The bot currently cannot connect to permanent hosting servers due to cost limitations. As a result, **all data may be wiped during maintenance**. However, **maintenance will no longer occur after full release**. Download buttons for attacks and defenses are available to help users save submissions manually.',
       footer: {
         text: 'Use !help anytime to get this link again.'
       }
